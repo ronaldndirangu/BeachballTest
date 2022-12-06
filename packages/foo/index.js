872966dba42914e8dedeb7fa13e1e979c495d8ca
Ronald Njuguna <rnjuguna@microsoft.com>
2022-12-06 09:45:06 +0300

    Add zoo package

--- packages/foo/index.js
+++ packages/foo/index.js
@@ -1,7 +1,12 @@
+const { getSumOfNumbersInArray } = require("@rn/zoo");
+
 const add = (a, b) => a + b;
 const subtract = (a, b) => a - b;
 
+const sumArray = (arr) => getSumOfNumbersInArray(arr);
+
 module.exports = {
     add,
-    subtract
+    subtract,
+    sumArray
 };
---
+++ packages/zoo/index.js
@@ -0,0 +1,9 @@
+const _ = require('lodash');
+
+const getSumOfNumbersInArray = (array) => {
+  return _.sum(array);
+}
+
+module.exports = {
+    getSumOfNumbersInArray
+}
